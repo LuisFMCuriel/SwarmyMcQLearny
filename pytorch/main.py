c7c22d9e8e91bf1c31aef338c06f3f81e7c8fd2a
LuisFMCuriel <felipemoralescuriel@gmail.com>
2023-08-29 16:53:02 +0200

    main.py: added main script for pytorch

---
+++ pytorch/main.py
@@ -0,0 +1,16 @@
+from strategies import EGreedyExpStrategy, GreedyStrategy
+from algorithms import DDQN
+
+
+my_DDQN = DDQN(gamma = 1,
+               env_name = 'CartPole-v1',
+               update_target_every_n_steps = 4,
+               training_strategy_fn = EGreedyExpStrategy(),
+               evaluation_strategy_fn = GreedyStrategy(),
+               lr = 0.005,
+               replay_buffer_size = 100000,
+               replay_buffer_batch_size = 64
+               )
+
+_, _ = my_DDQN.train(max_episodes = 10)
+my_DDQN.display_gif(filename = r"./media/CartPole-v1_pytorch.gif")
