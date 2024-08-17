2d5b54f40b7976b785d92fc5967f5ea5128bfec4
Luis <felipemoralescuriel@gmail.com>
2024-08-17 09:56:39 -0600

    added NormalNoiseStrategy

--- pytorch/strategies.py
+++ pytorch/strategies.py
@@ -44,4 +44,26 @@ class GreedyStrategy():
             # Only return the best choice for that state
             return np.argmax(q_values)
 
+class NormalNoiseStrategy():
+    def __init__(self, bounds, exploration_noise_ratio=0.1):
+        self.low, self.high = bounds
+        self.exploration_noise_ratio = exploration_noise_ratio
+        self.ratio_noise_injected = 0
+
+    def select_action(self, model, state, max_exploration=False):
+        if max_exploration:
+            noise_scale = self.high
+        else:
+            noise_scale = self.exploration_noise_ratio * self.high
+
+        with torch.no_grad():
+            greedy_action = model(state).cpu().detach().data.numpy().squeeze()
+
+        noise = np.random.normal(loc=0, scale=noise_scale, size=len(self.high))
+        noisy_action = greedy_action + noise
+        action = np.clip(noisy_action, self.low, self.high)
+
+        self.ratio_noise_injected = np.mean(abs((greedy_action - action)/(self.high - self.low)))
+        return action
+
 
